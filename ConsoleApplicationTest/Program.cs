c72c2cf2367c0920b0d2aabf54e6e76f70da845c
Srashti <Srashti@DESKTOP-LECFBRJ>
2017-02-17 01:07:23 +0100

    Add project files.

---
+++ ConsoleApplicationTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            localhost.WebServiceTest objet = new localhost.WebServiceTest(); 
+            Console.WriteLine("\n Enter the number n to get the fibonacci number at the nth position:  ");
+            int fibo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n " + objet.Fibonacci(fibo));
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
